0a87640352d7c14fa6ddd9897a95246216dec8ee
kr-younghoon <sullanta0802@gmail.com>
2024-07-18 22:01:22 +0900

    feat: Add rewrite rule for API endpoint

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async rewrites() {
+        return [
+            {
+                source: '/api/:slug*',
+                destination: 'http://43.203.8.51/:slug*',
+            },
+        ];
+    },
+};
 
 export default nextConfig;
