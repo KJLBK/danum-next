f2158a2e3fd580623e2753c08ae89823086fe9f0
kyeongb-bin <kyeongb0315@gmail.com>
2024-09-10 14:40:57 +0900

    feat: 질문이야기 게시판 기능 구현

--- src/app/questions/page.jsx
+++ src/app/questions/page.jsx
@@ -2,12 +2,14 @@ export default async function QuestionPage() {
   const token =
     "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhQGEiLCJyb2xlIjpbeyJhdXRob3JpdHkiOiJBRE1JTiJ9XSwiZXhwIjoyMDE2MjYyMzYyfQ.sUoNzSqQtO7A6eAOkUbCb4_lPL96i8xkIHyvI3X6TfU";
 
+  let posts = [];
+
   try {
-    let data = await fetch("/api/board/question/show", {
+    let data = await fetch("http://43.203.8.51:8080/board/question/show", {
       method: "GET",
       headers: { Authorization: `Bearer ${token}` },
     });
-    let posts = await data.json();
+    posts = await data.json();
   } catch (error) {
     console.error("Error fetching data", error);
   }
@@ -16,11 +18,11 @@ export default async function QuestionPage() {
       <h2>질문 이야기</h2>
       <ul>
         {posts.map((item) => (
-          <li key={item.id}>
-            <h2>{item.title}</h2>
-            <p>{item.content}</p>
+          <li key={item.question_id}>
+            <h2>제목:{item.title}</h2>
+            <p>내용:{item.content}</p>
             <p>작성자: {item.email}</p>
-            <p>작성 시간: {item.time}</p>
+            <p>작성 시간: {item.created_at}</p>
             <hr />
           </li>
         ))}
