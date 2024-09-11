6eaa9d27b113305b8d4566672dd1fb88dd7306ea
kr-younghoon <sullanta0802@gmail.com>
2024-09-11 10:21:20 +0900

    feat: Update login page to handle redirect after login
    - Add client-side import statement
    - Import necessary hooks and components
    - Implement redirect functionality after successful login
    
    Resolves: #10

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,9 +1,30 @@
+'use client';
+
+import { useEffect } from 'react';
 import LoginForm from '../../components/auth/LoginForm';
+import { useAuthStore } from '../../store/authStore';
+import {
+    useRouter,
+    useSearchParams,
+} from 'next/navigation';
+
 export default function LoginPage() {
+    const user = useAuthStore((state) => state.user);
+    const router = useRouter();
+    const searchParams = useSearchParams();
+    const redirect = searchParams.get('redirect') || '/';
+
+    useEffect(() => {
+        if (user) {
+            router.push(redirect);
+        }
+    }, [user, router, redirect]);
+
     return (
         <>
             <p>Login</p>
-            <LoginForm />
+
+            <LoginForm redirect={redirect} />
         </>
     );
 }
