8d019e6ddfb0cb26a5c03f21e2352f6df94e45ea
kr-younghoon <sullanta0802@gmail.com>
2024-09-11 19:41:39 +0900

    feat: Update layout and add header component
    - Update the title and description in the metadata of the RootLayout component
    - Change the language attribute of the html tag to 'kr'
    - Add a new Header component to the RootLayout

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <header>
+            <nav>
+                <div>danum</div>
+                <div>login</div>
+            </nav>
+        </header>
+    );
+}
