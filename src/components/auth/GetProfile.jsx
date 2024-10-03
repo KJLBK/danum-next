3fec0152c22bd1274a496c27ce209d8ca7475704
kyeongb-bin <kyeongb0315@gmail.com>
2024-10-03 18:29:09 +0900

    fix: img태그를 next Image태그로 수정

--- src/components/auth/GetProfile.jsx
+++ src/components/auth/GetProfile.jsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
+import Image from 'next/image';
 import { getProfile } from '../../service/authService';
 
 export default function GetProfile() {
@@ -29,12 +30,12 @@ export default function GetProfile() {
     }
     return (
         <div>
-            <img
+            <Image
                 src={profileImage}
                 alt='Profile'
+                width={30} // Next.js Image 컴포넌트에 필요한 width 속성
+                height={30} // Next.js Image 컴포넌트에 필요한 height 속성
                 style={{
-                    width: '30px', // 원하는 너비
-                    height: '30px', // 원하는 높이
                     objectFit: 'cover', // 이미지 비율 유지하면서 자르기
                     borderRadius: '50%', // 동그란 모양
                     border: '2px solid #ccc', // 선택사항: 테두리 추가
