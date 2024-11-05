248b6a166b166ebbfa02c2bfdb5262c9059e0270
kr-younghoon <sullanta0802@gmail.com>
2024-11-05 16:43:55 +0900

    test(test-page): Spinning 컴포넌트의 테스트를 위한 테스트 코드를 추가
    - 추후 삭제 예정

--- src/app/test/page.jsx
+++ src/app/test/page.jsx
@@ -1,55 +1,31 @@
-import PostInfoPanel from '../../components/board/view/PostInfoPanel';
-import QuillViewer from '../../components/board/view/QuillViewer';
+'use client';
 
-const QUESTIONS_DATA_DUMMY = [
-    {
-        question_id: 36,
-        title: 'How to implement chat functionality?',
-        content:
-            'I am struggling with implementing a chat feature using WebSocket. Can anyone help?',
-        author: {
-            userId: 'user123@example.com',
-            userName: 'TechLover',
-            profileImageUrl:
-                'https://example.com/profile1.png',
-        },
-        created_at: '2024-10-01T12:45:21.123456',
-        conversation: null,
-        like: 2,
-        view_count: 20,
-    },
-];
-const COMMENTS_DATA_DUMMY = [
-    {
-        comment_id: 59,
-        email: 'pep@pep',
-        content: 'jkbkjk',
-        created_at: '2024-10-19T02:32:22.689067',
-        accepted: false,
-    },
-    {
-        comment_id: 61,
-        email: 'Ehd@Ehd',
-        content: 'vnxznv,mxz',
-        created_at: '2024-10-19T17:53:04.602792',
-        accepted: false,
-    },
-    {
-        comment_id: 62,
-        email: 'Ehd@Ehd',
-        content: 'vmdn,a',
-        created_at: '2024-10-19T17:54:06.187184',
-        accepted: false,
-    },
-];
+import { useEffect, useState } from 'react';
+import Spinner from '../../components/common/Spinner';
 
 export default function Test() {
+    const [loading, setLoading] = useState(true);
+    // 3초 후에 로딩 상태를 false로 변경하여 Spinner가 사라지도록 합니다.
+    useEffect(() => {
+        const timer = setTimeout(
+            () => setLoading(false),
+            3000,
+        );
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
         <>
-            <PostInfoPanel data={QUESTIONS_DATA_DUMMY[0]} />
-            <QuillViewer
-                content={QUESTIONS_DATA_DUMMY[0].content}
-            />
+            <div
+                style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    height: '100vh',
+                }}
+            >
+                {loading ? <Spinner /> : <p>loaded!</p>}
+            </div>
         </>
     );
 }
