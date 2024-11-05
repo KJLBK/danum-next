e773ded76546de893e65edc8a0bebca592ce3710
kr-younghoon <sullanta0802@gmail.com>
2024-11-05 16:42:01 +0900

    chore(home): 기존 스타일 삭제 및 주석 추가
    - 기존 보일러플레이트 스타일과 기존 메인 페이지에 있었던 HTML5 태그를 삭제했습니다.
    - 간단한 영역 분리를 위한 HTML5 태그 추가와, 구현을 위한 주석을 추가하였습니다.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,21 @@
+import styles from './page.module.css';
+
 export default function Home() {
     return (
         <>
-            <h1>danum dev page</h1>
-            <div>(test build3)</div>
+            <main className={styles.mainAside}>
+                <aside className={styles.left}>
+                    {/* 로그인 전 */}
+                    {/* 로그인 후 */}
+                </aside>
+                <div className={styles.middle}>
+                    {/* 질문(무한스크롤) */}
+                </div>
+                <aside className={styles.right}>
+                    {/* 인기 게시물 */}
+                    {/* ??? */}
+                </aside>
+            </main>
         </>
     );
 }
