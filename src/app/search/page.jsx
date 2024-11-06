7aba6456ae64ff46e87f229718eb2a5d73d3aeae
kyeongb-bin <kyeongb0315@gmail.com>
2024-11-06 12:12:57 +0900

    feat: 검색 기능 쿼리 파라미터 수정

--- src/app/search/page.jsx
+++ src/app/search/page.jsx
@@ -1,31 +1,12 @@
 'use client';
 
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
+import { Suspense } from 'react';
+import SearchContent from '../../components/common/SearchContent';
 
 export default function SearchPage() {
-    const router = useRouter();
-    const [searchQuery, setSearchQuery] = useState('');
-
-    const handleSearch = (e) => {
-        setSearchQuery(e.target.value);
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        router.push(`/search?query=${searchQuery}`);
-    };
-
     return (
-        <>
-            <p>검색 페이지</p>
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="search"
-                    onChange={handleSearch}
-                    value={searchQuery}
-                />
-            </form>
-        </>
+        <Suspense fallback={<div>검색 중...</div>}>
+            <SearchContent />
+        </Suspense>
     );
 }
