e7e7cb62c44c8d44955263026db1e63f938f747c
kyeongb-bin <kyeongb0315@gmail.com>
2024-11-08 20:15:33 +0900

    feat: 모달 형식 적용

--- src/app/questions/page.jsx
+++ src/app/questions/page.jsx
@@ -1,11 +1,17 @@
+'use client';
 import { questionShow } from '../../services/postService.server';
 import style from './page.module.css'; // CSS 모듈 임포트
 import InfiniteScroll from '../../components/common/InfiniteScroll';
-
+import { useRouter } from 'next/navigation';
 export default function QuestionPage() {
+    const router = useRouter();
+    const handleNew = () => {
+        router.push('/questions/new');
+    };
     return (
         <div className={style.container}>
             <h2>질문 이야기</h2>
+            <button onClick={handleNew}>글쓰기</button>
             <InfiniteScroll
                 serviceLogic={questionShow}
                 queryKey={['questionPosts']}
