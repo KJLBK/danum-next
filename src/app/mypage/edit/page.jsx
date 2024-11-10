ab275143c4cc3c472dc1d6a207914d50127ed96a
kyeongb-bin <kyeongb0315@gmail.com>
2024-11-11 01:41:14 +0900

    feat: 수정할 프로필 데이터 패칭

--- src/app/mypage/edit/page.jsx
+++ src/app/mypage/edit/page.jsx
@@ -1,3 +1,49 @@
+'use client';
+import { useState, useEffect } from 'react';
+import { fetchUserData } from '../../../services/authService';
+// import Profile from '../../../components/auth/Profile';
+// import KakaoMap from '../../map/page';
+
 export default function ProfileEdit() {
-    return <div>edit</div>;
+    const [userData, setUserData] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const getUserData = async () => {
+            try {
+                setIsLoading(true);
+                const data = await fetchUserData();
+                setUserData(data);
+            } catch (err) {
+                setError(
+                    '사용자 데이터를 불러오는데 실패했습니다.',
+                );
+                console.error(
+                    'Error fetching user data:',
+                    err,
+                );
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        getUserData();
+    }, []);
+
+    if (isLoading) return <div>로딩 중...</div>;
+    if (error) return <div>{error}</div>;
+
+    return (
+        <div>
+            <form>
+                {/* <Profile />
+                이메일 :{' '}
+                <input type="text" value={userData.email} />
+                닉네임 :{' '}
+                <input type="text" value={userData.name} />
+                <KakaoMap /> */}
+            </form>
+        </div>
+    );
 }
