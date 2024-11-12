a100aefddc5f5db12e5a9a326de53cbaab19e947
kyeongb-bin <kyeongb0315@gmail.com>
2024-11-12 14:33:06 +0900

    feat: 지역별 글 조회 로직 구현

--- src/services/questionService.js
+++ src/services/questionService.js
@@ -314,3 +314,24 @@ export async function questionLike(questionId) {
         return [];
     }
 }
+
+// 지역별 글 조회 로직
+export async function regionShow(city, district) {
+    try {
+        const response = await fetch(
+            `/danum-backend/board/question/region?city=${city}district=${district}`,
+            {
+                method: 'GET',
+            },
+        );
+
+        if (!response.ok) {
+            throw new Error(`Error: ${response.status}`);
+        }
+
+        return await response.json();
+    } catch (error) {
+        console.error('Error fetching data', error);
+        return [];
+    }
+}
