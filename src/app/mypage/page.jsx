66d6be682653cd3e147d082d40e9d4bbbc045b32
kyeongb-bin <kyeongb0315@gmail.com>
2024-11-13 17:45:09 +0900

    feat: mypage 레이아웃 구성

--- src/app/mypage/page.jsx
+++ src/app/mypage/page.jsx
@@ -7,11 +7,15 @@ import {
 import BoardItem from '../../components/board/view/BoardItem';
 import { useAuthStore } from '../../stores/authStore';
 import { useRouter } from 'next/navigation';
+import styles from './page.module.css';
+import Image from 'next/image';
 
 export default function MypagePage() {
-    const { email } = useAuthStore();
+    const { email, exp, profileImageUrl, name } =
+        useAuthStore();
     const [questions, setQuestions] = useState([]);
     const [villages, setVillages] = useState([]);
+    const [activeTab, setActiveTab] = useState('question');
     const router = useRouter();
 
     useEffect(() => {
@@ -20,7 +24,6 @@ export default function MypagePage() {
                 const questionData =
                     await myQuestion(email);
                 const villageData = await myVillage(email);
-                // content 배열을 추출하여 설정
                 setQuestions(questionData.content || []);
                 setVillages(villageData.content || []);
             }
@@ -33,41 +36,128 @@ export default function MypagePage() {
         router.push('/mypage/edit');
     };
 
-    return (
-        <div className="mypage-container">
-            <button onClick={handleEdit}>
-                프로필 편집
-            </button>
-            <h2>내가 작성한 질문 글</h2>
-            <div>
-                {questions.map((question) => (
-                    <BoardItem
-                        key={question.question_id}
-                        question_id={question.question_id}
-                        title={question.title}
-                        content={question.content}
-                        author={question.author}
-                        created_at={question.created_at}
-                        view_count={question.view_count}
-                        board="questions"
-                    />
-                ))}
-            </div>
+    const renderContent = () => {
+        switch (activeTab) {
+            case 'question':
+                return questions.length > 0 ? (
+                    <div>
+                        {questions.map((question) => (
+                            <BoardItem
+                                key={question.question_id}
+                                question_id={
+                                    question.question_id
+                                }
+                                title={question.title}
+                                content={question.content}
+                                author={question.author}
+                                created_at={
+                                    question.created_at
+                                }
+                                view_count={
+                                    question.view_count
+                                }
+                                board="questions"
+                            />
+                        ))}
+                    </div>
+                ) : (
+                    <div className={styles.noContent}>
+                        <Image
+                            src="/eyes.png"
+                            alt="danum"
+                            width={100}
+                            height={75}
+                            priority={true}
+                        />
+                        <p>새로운 아티클이 없어요</p>
+                        <button
+                            onClick={() =>
+                                router.push('/new/question')
+                            }
+                        >
+                            새 아티클 쓰기
+                        </button>
+                    </div>
+                );
+            case 'village':
+                return villages.length > 0 ? (
+                    <div>
+                        {villages.map((village) => (
+                            <BoardItem
+                                key={village.village_id}
+                                village_id={
+                                    village.village_id
+                                }
+                                title={village.title}
+                                content={village.content}
+                                author={village.author}
+                                created_at={
+                                    village.created_at
+                                }
+                                view_count={
+                                    village.view_count
+                                }
+                                board="villages"
+                            />
+                        ))}
+                    </div>
+                ) : (
+                    <div className={styles.noContent}>
+                        <Image
+                            src="/eyes.png"
+                            alt="danum"
+                            width={100}
+                            height={75}
+                            priority={true}
+                        />
+                        <p>새로운 마을 글이 없어요</p>
+                        <button
+                            onClick={() =>
+                                router.push('/new/village')
+                            }
+                        >
+                            새 글 쓰기
+                        </button>
+                    </div>
+                );
+            default:
+                return null;
+        }
+    };
 
-            <h2>내가 작성한 마을 글</h2>
-            <div>
-                {villages.map((village) => (
-                    <BoardItem
-                        key={village.village_id}
-                        village_id={village.village_id}
-                        title={village.title}
-                        content={village.content}
-                        author={village.author}
-                        created_at={village.created_at}
-                        view_count={village.view_count}
-                        board="villages"
-                    />
-                ))}
+    return (
+        <div className={styles.infoContainer}>
+            <div className={styles.profileTop}>
+                <Image
+                    src={
+                        profileImageUrl ||
+                        '/logo-assets/android-chrome-512x512.png'
+                    }
+                    width={64}
+                    height={64}
+                    alt={`image`}
+                />
+                <button onClick={handleEdit}>
+                    프로필 편집
+                </button>
+            </div>
+            <h2 className={styles.name}>{name}</h2>
+            <p className={styles.email}>{email}</p>
+
+            <div className={styles.boardCategory}>
+                <button
+                    onClick={() => setActiveTab('question')}
+                >
+                    질문
+                </button>
+                <button
+                    onClick={() => setActiveTab('village')}
+                >
+                    마을
+                </button>
+            </div>
+            <div className={styles.boardContent}>
+                {renderContent()}
             </div>
         </div>
     );
