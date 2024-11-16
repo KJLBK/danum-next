5728cecff4cc6ba33bd2e3739dd18c1492456a51
kyeongb-bin <kyeongb0315@gmail.com>
2024-11-16 17:09:25 +0900

    feat: BoardItem 디자인

--- src/components/common/InfiniteScroll.jsx
+++ src/components/common/InfiniteScroll.jsx
@@ -116,6 +116,7 @@ export default function InfiniteScroll({
                                     post.hasAcceptedComment
                                 }
                                 board={board}
+                                postType={post.postType}
                             />
                         );
                     })}
